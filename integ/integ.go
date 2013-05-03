755a1221251c0b4a632b673ca323b1ccdbe82646
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-05-03 01:47:34 -0400

    First pass at integration routine.

--- integ/integ.go
+++ integ/integ.go
@@ -19,8 +19,8 @@ static gsl_function mkintegCB(void *data) {
 import "C"
 
 import (
-	"errors"
 	"github.com/npadmana/gslgo"
+	"math"
 	"unsafe"
 )
 
@@ -30,16 +30,51 @@ func integCB(x C.double, data unsafe.Pointer) C.double {
 	return C.double(ff.Gofunc(float64(x)))
 }
 
+// Workspace is the GSL integration workspace
 type WorkSpace struct {
+	n int
 	w *C.gsl_integration_workspace
 }
 
+// NewWork allocate new workspace
 func NewWork(n int) *WorkSpace {
 	ret := new(WorkSpace)
-	ret.w = C.gsl_integration_workspace_alloc(C.int(n))
+	ret.n = n
+	ret.w = C.gsl_integration_workspace_alloc(C.size_t(n))
 	return ret
 }
 
+// Free frees workspace w
 func (w *WorkSpace) Free() {
 	C.gsl_integration_workspace_free(w.w)
+}
+
+func Qags(ff gslgo.F, ab gslgo.Interval, eps gslgo.Eps, w *WorkSpace) (gslgo.Result, error) {
+	// Make a gsl_function
+	var gf C.gsl_function
+	data := gslgo.GSLFuncWrapper{ff}
+	gf = C.mkintegCB(unsafe.Pointer(&data))
+
+	// Check to see if we have a positive/-negative infinity
+	pinf := math.IsInf(ab.Hi, 1)
+	ninf := math.IsInf(ab.Lo, -1)
+
+	var ret C.int
+	var y, err C.double
+	// Switch on options
+	switch {
+	case pinf && ninf:
+		ret = C.gsl_integration_qagi(&gf, C.double(eps.Abs), C.double(eps.Rel), C.size_t(w.n), w.w, &y, &err)
+	case pinf:
+		ret = C.gsl_integration_qagiu(&gf, C.double(ab.Lo), C.double(eps.Abs), C.double(eps.Rel), C.size_t(w.n), w.w, &y, &err)
+	case ninf:
+		ret = C.gsl_integration_qagil(&gf, C.double(ab.Hi), C.double(eps.Abs), C.double(eps.Rel), C.size_t(w.n), w.w, &y, &err)
+	default:
+		ret = C.gsl_integration_qags(&gf, C.double(ab.Lo), C.double(ab.Hi), C.double(eps.Abs), C.double(eps.Rel), C.size_t(w.n), w.w, &y, &err)
+	}
+	if ret != 0 {
+		return gslgo.Result{float64(y), float64(err)}, gslgo.Errno(ret)
+	}
+	return gslgo.Result{float64(y), float64(err)}, nil
+
 }
